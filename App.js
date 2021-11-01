c0925b85604ef08192f82d8f9eef8ba2f3d7388c
AyoTheInspired <ayoinspired@gmail.com>
2021-11-01 11:33:08 +0100

    Initial commit

--- App.js
+++ App.js
@@ -5,7 +5,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Open up App.js to start working on your app from the laptop that is
+        still checking seeing it here! check seeing it on checking again styles
+        only one again. seeing again checking for changes state
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
