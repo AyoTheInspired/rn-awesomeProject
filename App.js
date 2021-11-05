0934e6a0231b79407241232c2057173fbd26965b
AyoTheInspired <ayoinspired@gmail.com>
2021-11-05 14:26:03 +0100

    Component Reusability

--- App.js
+++ App.js
@@ -1,25 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Platform,
+  StatusBar,
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
 
-export default function App() {
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>
-        Open up App.js to start working on your app from the laptop that is
-        still checking seeing it here! check seeing it on checking again styles
-        only one again. seeing again checking for changes state
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Here are some boxes of different colors</Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.boxText}>Cyan: #2aa198</Text>
+        </View>
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.boxText}>Blue: #268bd2</Text>
+        </View>
+        <View style={[styles.box, styles.magenta]}>
+          <Text style={styles.boxText}>Magenta: #d33682</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.boxText}>Orange: #cb4b16</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 10,
+  },
+
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  box: {
+    padding: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  cyan: {
+    backgroundColor: '#2aa198',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  blue: {
+    backgroundColor: '#268bd2',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  magenta: {
+    backgroundColor: '#d33682',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  orange: {
+    backgroundColor: '#cb4b16',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+
+  AndroidSafeArea: {
+    paddingTop: StatusBar.currentHeight || 0,
   },
 });
